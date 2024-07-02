0148b77ae7349ba4c9ee520eb5b5b74d6dc0308e
ECM1966 <ernesto.calvo.moreno@gmail.com>
2024-07-02 15:51:14 +0200

    setup conexion_1

---
+++ modelo/conexion_cliente.php
@@ -0,0 +1,18 @@
+<?php
+//conexion.php
+
+function conectarBD(){
+    $host = 'localhost';
+    $dbname = 'agenda';
+    $username = 'root';
+    $password = '';
+
+    try{
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e){
+        die("Error al conectar con la base de datos: " . $e->getMessage());
+    }
+}
+?>
